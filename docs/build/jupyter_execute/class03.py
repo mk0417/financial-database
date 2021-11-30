d55ad0ca4fe1fd8b0e17eaeb89834200b32838b8
“mk0417” <pengdata1@gmail.com>
2021-11-30 20:13:14 +0000

    update

--- docs/build/jupyter_execute/class03.py
+++ docs/build/jupyter_execute/class03.py
@@ -1,5 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import pandas as pd
 import numpy as np
+
+
+# In[2]:
+
 
 url = 'https://www.dropbox.com/s/qro99n7ngq0aztr/funda_raw.txt?dl=1'
 funda_raw = pd.read_csv(url, sep='\t', low_memory=False)
@@ -7,8 +17,16 @@ funda_raw = pd.read_csv(url, sep='\t', low_memory=False)
 # View first 5 rows and first 5 columns
 funda_raw.iloc[:5, :5]
 
+
+# In[3]:
+
+
 # Data types
 funda_raw.info()
+
+
+# In[4]:
+
 
 # Read the data again with consideration of the data type of gvkey
 funda_raw = pd.read_csv(url, sep='\t', low_memory=False, dtype={'gvkey': str})
@@ -16,8 +34,16 @@ funda_raw = pd.read_csv(url, sep='\t', low_memory=False, dtype={'gvkey': str})
 # View first 5 rows and first 5 columns
 funda_raw.iloc[:5, :5]
 
+
+# In[5]:
+
+
 # Data types
 funda_raw.info()
+
+
+# In[6]:
+
 
 funda_raw = funda_raw.sort_values(['gvkey', 'fyear', 'datadate'], ignore_index=True)
 print(f'Number of obs: {len(funda_raw)}')
@@ -38,26 +64,58 @@ funda = funda.drop(columns=drop_vars)
 funda = funda.query('at>0 & seq>0').copy()
 print(f'Number of obs after requiring positive asset: {len(funda)}')
 
+
+# In[7]:
+
+
 funda['exchg'].value_counts()
+
+
+# In[8]:
+
 
 funda['date'] = pd.to_datetime(funda['datadate'], format='%Y%m%d')
 funda['month'] = funda['date'].dt.month
 funda['month'].value_counts()
 
+
+# In[9]:
+
+
 funda['roa'] = funda['ebit'] / funda['at']
 funda['roe'] = funda['ebit'] / funda['seq']
+
+
+# In[10]:
+
 
 funda = funda.sort_values(['gvkey', 'fyear'], ignore_index=True)
 funda['lag_at'] = funda.groupby('gvkey')['at'].shift(1)
 funda['lag_fyear'] = funda.groupby('gvkey')['fyear'].shift(1)
 funda['ag'] = funda['at'] / funda['lag_at'] - 1
 
+
+# In[11]:
+
+
 funda['fyear_diff'] = funda['fyear'] - funda['lag_fyear']
 funda.query('fyear_diff>1')[['gvkey', 'fyear', 'lag_fyear', 'fyear_diff', 'ag']].head()
 
+
+# In[12]:
+
+
 funda.loc[funda['fyear_diff']>1, 'ag'] = np.nan
 
+
+# In[13]:
+
+
 round(funda[['roa', 'roe', 'ag']].describe(), 3)
+
+
+# In[14]:
+
 
 # Winsorising at 1% and 99% percentile
 for i in ['roa', 'roe', 'ag']:
@@ -68,3 +126,4 @@ for i in ['roa', 'roe', 'ag']:
     funda = funda.drop(columns=['p1', 'p99'])
 
 round(funda[['roa', 'roe', 'ag']].describe(), 3)
+
